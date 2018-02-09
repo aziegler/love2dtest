26e5356a9976bde34e0b33a68fe0eeda26748333
Axelle Ziegler <axelle.ziegler@gmail.com>
2018-02-09 19:33:18 +0100

    Extract constants

--- main.lua
+++ main.lua
@@ -5,19 +5,26 @@ map.content = {}
 map.overlay = {}
 map.sizeX = 20
 map.sizeY = 15
-map.tileColumn = 18
-map.tileSize = 126
 map.posX = 0
 map.posY = 0
 
 local currentTile = 1
 local possibleTiles = {15,16,17,18}
 
+local constants = {}
+constants.spriteSheet = "assets/Tilesheet/medieval_tilesheet.png"
+constants.tileSize = 64
+constants.tileMargin = 32
+constants.mapSize = 100
+constants.spriteSheetHeight = 704
+constants.spriteSheetWidth = 1760
+constants.spriteSheetColumn = 18
+
 function love.load()
-	spriteSheet = love.graphics.newImage("assets/Tilesheet/medieval_tilesheet.png")
+	spriteSheet = love.graphics.newImage(constants.spriteSheet)
 	love.window.setFullscreen(true)
-	map.tilesCountX = math.floor(love.graphics.getWidth() / 64)
-	map.tilesCountY = math.floor(love.graphics.getHeight() / 64)
+	map.tilesCountX = math.floor(love.graphics.getWidth() / constants.tileSize)
+	map.tilesCountY = math.floor(love.graphics.getHeight() / constants.tileSize)
 	mapFile = io.open("assets/map.tmx", "r")
 	local i = 0
 	for line in mapFile:lines() do
@@ -44,26 +51,32 @@ end
 
 function love.update()	
 	if love.keyboard.isDown("z") then
-		map.posX = math.min(map.posX + 1,100 - map.tilesCountX -1)
+		map.posX = math.min(map.posX + 1,constants.mapSize - map.tilesCountX -1)
 	elseif love.keyboard.isDown("s") then
-		map.posY = math.min(map.posY + 1,100 - map.tilesCountY - 1)
+		map.posY = math.min(map.posY + 1,constants.mapSize - map.tilesCountY - 1)
 	elseif love.keyboard.isDown("q") then
 		map.posX = math.max(map.posX - 1,0)
 	elseif love.keyboard.isDown("d") then 
 		map.posY = math.max(map.posY - 1,0)
 	end
 	if(love.mouse.isDown(1)) then
-		map.overlay[math.floor(love.mouse.getY()/64)][math.floor(love.mouse.getX()/64)] = possibleTiles[currentTile]
+		map.overlay[math.floor(love.mouse.getY()/constants.tileSize)][math.floor(love.mouse.getX()/constants.tileSize)] = possibleTiles[currentTile]
 	end
 
 end
 
 function build_quad(x,y)
-	return  love.graphics.newQuad(32 + x * 96, 32 + 96 * y, 64, 64,1760,704)
+	return  love.graphics.newQuad(
+		constants.tileMargin + x * ( constants.tileMargin + constants.tileSize), 
+		constants.tileMargin + ( constants.tileMargin + constants.tileSize) * y, 
+		constants.tileSize, 
+		constants.tileSize,
+		constants.spriteSheetWidth,
+		constants.spriteSheetHeight)
 end
 
 function draw_exact_tile(x,y,tileNumber)
-	love.graphics.draw(spriteSheet,build_quad ((tileNumber-1) % 18,math.floor(tileNumber / 18)), x * 64, y * 64)	
+	love.graphics.draw(spriteSheet,build_quad ((tileNumber-1) % constants.spriteSheetColumn,math.floor(tileNumber / constants.spriteSheetColumn)), x * constants.tileSize, y * constants.tileSize)	
 end
 
 function draw_tile(x,y,tileNumber)
@@ -87,5 +100,5 @@ function love.draw()
 			end
 		end
 	end
-	draw_exact_tile(math.floor(love.mouse.getX()/64),math.floor(love.mouse.getY()/64),possibleTiles[currentTile])
+	draw_exact_tile(math.floor(love.mouse.getX()/constants.tileSize),math.floor(love.mouse.getY()/constants.tileSize),possibleTiles[currentTile])
 end 
